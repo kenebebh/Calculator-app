d286c97a984933ac7a5dc6a1a5e571a23ca12417
Banigo Kenebebh <kenebebhbanigo@gmail.com>
2022-08-30 14:01:56 +0100

    implemented the simple functionalities

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const storageContainer = document.querySelector(".storage-container");
-const prevOperation = document.querySelector(".previous-operation");
-const currentOperation = document.querySelector(".current-operation");
+const prevOperationEl = document.querySelector(".previous-operation");
+const currentOperationInput = document.querySelector(".current-operation");
 
 const numberButtons = document.querySelectorAll(".btn-number");
 const operatorButtons = document.querySelectorAll(".btn-operator");
@@ -8,19 +8,28 @@ const clearButton = document.querySelector(".btn-all-clear");
 const deleteButton = document.querySelector(".btn-delete");
 const numberSquaredButton = document.querySelector(".btn-square");
 const squareRootButton = document.querySelector(".btn-square-root");
-const equalsButton = document.querySelector(".btn-all-equals");
+const equalsButton = document.querySelector(".btn-equals");
 
 class CalculatorApp {
-  constructor() {
-    // this.clear();
+  constructor(prevOperationEl, currentOperationInput) {
+    this.prevOperationEl = prevOperationEl;
+    this.currentOperationInput = currentOperationInput;
+    this.clear();
   }
 
-  // clear() {
-  //     currentOperation.value = ''
-  // }
+  clear() {
+    this.currentOperation = "";
+    this.previousOperation = "";
+    this.operation = undefined;
+  }
+
+  delete() {
+    this.currentOperation = this.currentOperation.toString().slice(0, -1);
+  }
 
   displayNumber(number) {
-    currentOperation.value += number;
+    if (number === "." && this.currentOperation.includes(".")) return;
+    this.currentOperation += number;
   }
 
   chooseOperation(operation) {
@@ -29,43 +38,73 @@ class CalculatorApp {
       this.compute();
     }
     this.operation = operation;
-    this.previousOperand = this.currentOperand;
-    this.currentOperand = "";
+    this.previousOperation = this.currentOperation;
+    this.currentOperation = "";
   }
 
   compute() {
     let computation;
-
+    console.log(this.prevOperation);
+    const prev = parseFloat(this.previousOperation);
+    const current = parseFloat(this.currentOperation);
+    console.log(prev, current);
+    if (isNaN(prev) || isNaN(current)) return;
     switch (this.operation) {
       case "+":
-        console.log(Add);
+        computation = prev + current;
         break;
       case "-":
-        console.log(Subtract);
+        computation = prev - current;
         break;
       case "x":
-        console.log(Multiply);
+        computation = prev * current;
         break;
       case "÷":
-        console.log(Divide);
+        computation = prev / current;
         break;
       default:
         return;
     }
+    this.currentOperation = computation;
+    this.operation = undefined;
+    this.previousOperation = "";
+  }
+
+  updateDisplay() {
+    this.currentOperationInput.value = this.currentOperation;
+    this.prevOperationEl.innerText = this.previousOperation;
   }
 }
 
-const calculator = new CalculatorApp();
+const calculator = new CalculatorApp(prevOperationEl, currentOperationInput);
+console.log(calculator);
 
 numberButtons.forEach((button) => {
   button.addEventListener("click", () => {
     calculator.displayNumber(button.innerText);
+    calculator.updateDisplay();
   });
 });
 
 operatorButtons.forEach((button) => {
   button.addEventListener("click", () => {
     calculator.chooseOperation(button.innerText);
-    calculator.displayNumber(button.innerText);
+    // calculator.displayNumber(button.innerText);
+    calculator.updateDisplay();
   });
+});
+
+equalsButton.addEventListener("click", function () {
+  calculator.compute();
+  calculator.updateDisplay();
+});
+
+clearButton.addEventListener("click", function () {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+
+deleteButton.addEventListener("click", function () {
+  calculator.delete();
+  calculator.updateDisplay();
 });
