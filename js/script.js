6824c4ecf78262f867ba16b3a55e5f91c71b26a4
Banigo Kenebebh <kenebebhbanigo@gmail.com>
2022-08-30 03:05:32 +0100

    trying to implement the calculating function

--- js/script.js
+++ js/script.js
@@ -6,7 +6,10 @@ const clearButton = document.querySelector(".btn-all-clear");
 const deleteButton = document.querySelector(".btn-delete");
 const numberSquaredButton = document.querySelector(".btn-square");
 const squareRootButton = document.querySelector(".btn-square-root");
-const equalsButton = document.querySelector(".btn-all-equals");
+const equalsButton = document.querySelector(".btn-equals");
+let prevOperand, currOperand;
+
+console.log(numberSquaredButton.innerText, squareRootButton.innerHTML);
 
 class CalculatorApp {
   constructor() {
@@ -18,34 +21,40 @@ class CalculatorApp {
   // }
 
   displayNumber(number) {
+    if (number === "." && displayBar.value.includes(".")) return;
     displayBar.value += number;
   }
 
-  chooseOperation(operation) {
-    if (this.currentOperand === "") return;
-    if (this.previousOperand !== "") {
-      this.compute();
-    }
+  setOperation(operation) {
     this.operation = operation;
-    this.previousOperand = this.currentOperand;
-    this.currentOperand = "";
+    this.compute();
   }
 
-  compute() {
-    let computation;
+  //   chooseOperation(operation) {
+  //     this.compute();
+  //   }
 
+  compute() {
+    let computed;
+    const calcthis = displayBar.value;
+    const splitString = calcthis.match(/(?:\d+\.)?\d+/g);
+    prevOperand = parseFloat(splitString[0]);
+    currOperand = parseFloat(splitString[1]);
     switch (this.operation) {
       case "+":
-        console.log(Add);
+        console.log(`Add`);
+        computed = prevOperand + currOperand;
+        console.log(computed);
+        prevOperand = computed;
         break;
       case "-":
-        console.log(Subtract);
+        console.log(`Subtract`);
         break;
       case "x":
-        console.log(Multiply);
+        console.log(`Multiply`);
         break;
       case "÷":
-        console.log(Divide);
+        console.log(`Divide`);
         break;
       default:
         return;
@@ -63,7 +72,12 @@ numberButtons.forEach((button) => {
 
 operatorButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    calculator.chooseOperation(button.innerText);
+    calculator.setOperation(button.innerText);
+    // calculator.compute(button.innerText);
     calculator.displayNumber(button.innerText);
   });
+});
+
+equalsButton.addEventListener("click", function () {
+  calculator.compute();
 });
